1f85e63e386dd8ea6337eba5d1be6f23faae9e60
Hyunwoo-Seo <shw1801@naver.com>
2025-05-08 16:22:38 +0900

    fix: 클라우드 타입에서 배포 후 코드 수정

--- server.js
+++ server.js
@@ -5,26 +5,28 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 
+const PORT = process.env.PORT || 3001;
+
 const io = new Server(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("✅ 클라이언트가 연결되었습니다.");
+
+  socket.on("chat message", (msg) => {
+    console.log("수신된 메시지:", msg);
+    io.emit("메세지", msg);
   });
 
-  io.on("connection", (socket) => {
-    console.log("연결되었습니다.");
-  
-    socket.on("chat message", (msg) => {
-      console.log("메세지 : ", msg);
-      io.emit("메세지", msg);
-    });
-  
-    socket.on("disconnect", () => {
-      console.log("연결이 끊어졌습니다.");
-    });
+  socket.on("disconnect", () => {
+    console.log("클라이언트 연결이 끊어졌습니다.");
   });
+});
 
-  server.listen(3001, () => {
-    console.log("3001번 포트에서 연결되었습니다.");
-  });
+server.listen(PORT, () => {
+  console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
+});
